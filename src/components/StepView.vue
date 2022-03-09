131afb9f2fff7874d2860bfa7d5bafc9c4d33977
GinJack <jack90928@gmail.com>
2022-03-09 17:54:41 +0800

    update CartView get cart's datas and complete change qty

---
+++ src/components/StepView.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="row jy-content-between align-items-center py-18 mb-10">
+    <div class="col-lg-3">
+      <h3 class="text-title">
+        <span class="text-c-forthary">全台宅配</span>
+        <span class="mx-2">|</span>
+        <span>購物車</span>
+      </h3>
+    </div>
+    <div class="col-lg-6">
+      <div class="row">
+        <div class="col-lg-4">
+          <div class="step-item active">
+            <h3 class="text-medium">
+              <span class="active">1</span>
+              明細消費
+            </h3>
+            <div class="hide-bar active"></div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="step-item">
+            <h3 class="text-medium">
+              <span>2</span>
+              填寫資料
+            </h3>
+            <div class="hide-bar"></div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="step-item">
+            <h3 class="text-medium">
+              <span>3</span>
+              購買完成
+            </h3>
+            <div class="hide-bar"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+.step-item {
+  min-height: 60px;
+  background-color: #c1c1c1;
+  position: relative;
+  .hide-bar {
+    width: 0;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    transform: translateX(100%);
+    border-style: solid;
+    border-width: 30px 0 30px 14px;
+    border-color: transparent transparent transparent #c1c1c1;
+    &.active {
+      border-color: transparent transparent transparent $secondary;
+    }
+  }
+  h3 {
+    color: white;
+    text-align: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    line-height: 60px;
+    span {
+      width: 20px;
+      height: 20px;
+      background: white;
+      border-radius: 20px;
+      margin-right: 4px;
+      font-size: 12px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-weight: bold;
+      color: #c1c1c1;
+      &.active {
+        color: $secondary;
+      }
+    }
+  }
+  &.active {
+    background-color: $secondary;
+  }
+}
+</style>
--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -1,46 +1,8 @@
 <template>
 <div class="container">
+  <LoadingView :active="isLoading"></LoadingView>
   <div class="cart">
-    <div class="row jy-content-between align-items-center py-18 mb-10">
-      <div class="col-lg-3">
-        <h3 class="text-title">
-          <span class="text-c-forthary">全台宅配</span>
-          <span class="mx-2">|</span>
-          <span>購物車</span>
-        </h3>
-      </div>
-      <div class="col-lg-6">
-        <div class="row">
-          <div class="col-lg-4">
-            <div class="step-item active">
-              <h3 class="text-medium">
-                <span class="active">1</span>
-                明細消費
-              </h3>
-              <div class="hide-bar active"></div>
-            </div>
-          </div>
-          <div class="col-lg-4">
-            <div class="step-item">
-              <h3 class="text-medium">
-                <span>2</span>
-                填寫資料
-              </h3>
-              <div class="hide-bar"></div>
-            </div>
-          </div>
-          <div class="col-lg-4">
-            <div class="step-item">
-              <h3 class="text-medium">
-                <span>3</span>
-                購買完成
-              </h3>
-              <div class="hide-bar"></div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <StepView></StepView>
     <ul class="mb-10">
       <li class="row py-4 bg-secondary">
         <div class="col-lg-4">
@@ -59,29 +21,42 @@
           <p class="text-center text-c-white">操作</p>
         </div>
       </li>
-      <li class="row py-4 bg-white align-items-center border-b-1">
+      <li
+        class="row py-4 bg-white align-items-center border-b-1"
+        v-for="item in carts.carts"
+        :key="item.id"
+      >
         <div class="col-lg-4 d-flex jy-content-center">
-          <div class="img-bg px-1" style="background-image: url(https://www.funthingtrue.com.tw/upload/product/20220210113009_p1.jpg)"></div>
+          <div
+            class="img-bg px-1"
+            :style="{backgroundImage: 'url('+item.product.imageUrl+')'}"
+          ></div>
           <div class="content px-3">
-            <h3 class="text-medium">嘎啦脆爆米花心滿意足12件組</h3>
+            <h3 class="text-medium">{{ item.product.title }}</h3>
             <p>
-              規格：12包<br>
-              重量：0.72 kg ±10%
+              規格：{{ item.product.unit }}<br>
+              重量：{{ item.product.content }}
             </p>
           </div>
         </div>
         <div class="col-lg-2">
-          <select class="py-2" name="qty" id="qty">
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
+          <select
+            class="py-2" name="qty" id="qty"
+            v-model="item.qty"
+            @change="updateCart(item, item.qty)"
+          >
+            <option
+              v-for="(qty, key) in 20"
+              :key="qty + key"
+              :value="qty"
+            >{{ qty }}</option>
           </select>
         </div>
         <div class="col-lg-2">
-          <p class="text-medium text-center text-c-primary">NT $990</p>
+          <p class="text-medium text-center text-c-primary">NT ${{ item.product.price }}</p>
         </div>
         <div class="col-lg-2">
-          <p class="text-medium text-center text-c-primary">NT $990</p>
+          <p class="text-medium text-center text-c-primary">NT ${{ item.total }}</p>
         </div>
         <div class="col-lg-2">
           <button class="user-btn user-btn-primary m-auto py-1 px-2">
@@ -91,36 +66,12 @@
           </button>
         </div>
       </li>
-      <li class="row py-4 bg-white align-items-center border-b-1">
-        <div class="col-lg-4 d-flex jy-content-center">
-          <div class="img-bg px-1" style="background-image: url(https://www.funthingtrue.com.tw/upload/product/20220210113009_p1.jpg)"></div>
-          <div class="content px-3">
-            <h3 class="text-medium">嘎啦脆爆米花心滿意足12件組</h3>
-            <p>
-              規格：12包<br>
-              重量：0.72 kg ±10%
-            </p>
-          </div>
+      <li class="row py-4 bg-white align-items-center border-b-1 jy-content-end">
+        <div class="col-lg-2">
+          <p class="text-medium text-center text-c-primary">總計</p>
         </div>
         <div class="col-lg-2">
-          <select class="py-2" name="qty" id="qty">
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-          </select>
-        </div>
-        <div class="col-lg-2">
-          <p class="text-medium text-center text-c-primary">NT $990</p>
-        </div>
-        <div class="col-lg-2">
-          <p class="text-medium text-center text-c-primary">NT $990</p>
-        </div>
-        <div class="col-lg-2">
-          <button class="user-btn user-btn-primary m-auto py-1 px-2">
-            <span class="text-c-white">
-              X
-            </span>
-          </button>
+          <p class="text-medium text-center text-c-primary f-size-s">NT ${{ carts.final_total }}</p>
         </div>
       </li>
     </ul>
@@ -129,8 +80,50 @@
 </template>
 
 <script>
+import StepView from '@/components/StepView.vue'
 export default {
-
+  data () {
+    return {
+      isLoading: false,
+      carts: []
+    }
+  },
+  components: {
+    StepView
+  },
+  methods: {
+    getCarts () {
+      this.isLoading = true
+      this.$http
+        .get(`${process.env.VUE_APP_APIURL}/api/${process.env.VUE_APP_PATH}/cart`)
+        .then((res) => {
+          this.isLoading = false
+          this.carts = res.data.data
+          this.$emitter.emit('updateCart', res.data.data)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    },
+    updateCart (item, qty) {
+      this.isLoading = true
+      const data = {
+        product_id: item.product_id,
+        qty: qty
+      }
+      this.$http
+        .put(`${process.env.VUE_APP_APIURL}/api/${process.env.VUE_APP_PATH}/cart/${item.id}`, { data })
+        .then((res) => {
+          this.getCarts()
+        })
+        .catch((err) => {
+          console.dir(err)
+        })
+    }
+  },
+  mounted () {
+    this.getCarts()
+  }
 }
 </script>
 
@@ -139,52 +132,6 @@ export default {
     padding-top: 60px;
     &-title{
       padding: 5% 0;
-    }
-    .step-item{
-      min-height: 60px;
-      background-color: #C1C1C1;
-      position: relative;
-      .hide-bar{
-        width: 0;
-        position: absolute;
-        top: 0;
-        bottom: 0;
-        right: 0;
-        transform: translateX(100%);
-        border-style: solid;
-        border-width: 30px 0 30px 14px;
-        border-color: transparent transparent transparent #C1C1C1;
-        &.active{
-          border-color: transparent transparent transparent $secondary;
-        }
-      }
-      h3{
-        color: white;
-        text-align: center;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        line-height: 60px;
-        span{
-          width: 20px;
-          height: 20px;
-          background: white;
-          border-radius: 20px;
-          margin-right: 4px;
-          font-size: 12px;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          font-weight: bold;
-          color: #C1C1C1;
-          &.active{
-            color: $secondary;
-          }
-        }
-      }
-      &.active{
-        background-color: $secondary;
-      }
     }
     .img-bg{
       background-size: cover;
