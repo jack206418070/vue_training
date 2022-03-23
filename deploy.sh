a51b8c43d05e85809384ac23f6f584f1f2c0da39
GinJack <jack90928@gmail.com>
2022-03-23 12:38:48 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -16,7 +16,7 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f https://github.com/jack206418070/vue_training.git master:gh-pages
+git push -f https://github.com/jack206418070/vue_training.git main:gh-pages
 
 # git push -f https://github.com/jack206418070/vue-week6.git master:gh-pages
 
